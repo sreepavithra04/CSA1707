a0d57ff12fa45ebfa2f56bd62fc9afa787082ba0
Sree Pavithra Reddy <vanukurisreepavithra@gmail.com>
2025-09-13 15:15:09 +0530

    Delete 17.py

--- 17.py
+++
@@ -1,7 +0,0 @@
-% sum(N, Result) :- Result is the sum of integers from 1 to N.
-sum(0, 0).  % Base case: sum of numbers up to 0 is 0
-sum(N, Result) :-
-    N > 0,
-    N1 is N - 1,
-    sum(N1, Partial),
-    Result is Partial + N.
